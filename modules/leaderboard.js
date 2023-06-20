2daa1ac641ab4e519320c1638586968a79573840
rbd3 <rabedesanaa@gmail.com>
2023-06-20 12:02:47 +0300

    Fix linters errors

--- modules/leaderboard.js
+++ modules/leaderboard.js
@@ -1,15 +1,15 @@
 export default class Leaderboard {
     constructor() {
-        this.scores = {
-            id: "",
-            names: "",
-            score: "",
-        };
+     this.scores = {
+        id: "",
+        names: "",
+        score: "",
+     };
     }
 
     add(names, score) {
-        this.id = this.scores.lenght + 1;
-        this.names = names;
-        this.score = score;
-    }
+     this.id = this.scores.lenght + 1;
+     this.names = names;
+     this.score = score;
+ }
 }
