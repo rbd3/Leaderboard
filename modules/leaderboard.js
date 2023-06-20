ff89382d395962fc93245f7604fbe518a045745f
rbd3 <rabedesanaa@gmail.com>
2023-06-20 11:51:04 +0300

    Add html and css code for the basic need

---
+++ modules/leaderboard.js
@@ -0,0 +1,15 @@
+export default class Leaderboard {
+    constructor() {
+        this.scores = {
+            id: "",
+            names: "",
+            score: "",
+        };
+    }
+
+    add(names, score) {
+        this.id = this.scores.lenght + 1;
+        this.names = names;
+        this.score = score;
+    }
+}
