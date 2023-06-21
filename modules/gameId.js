ad3bdd5a6a6e0b7b421827b296c919e8742a853e
rbd3 <rabedesanaa@gmail.com>
2023-06-21 17:59:19 +0300

    Fix linters errors

--- modules/gameId.js
+++ modules/gameId.js
@@ -4,7 +4,7 @@ const gameId = () => {
     {
       method: 'POST',
       body: JSON.stringify({
-       name: 'Project Game',
+        name: 'Project Game',
       }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
@@ -14,7 +14,7 @@ const gameId = () => {
     .then((response) => response.json())
     .then((json) => {
       const gameId = json.result.match(/Game with ID: (\w+) added/)[1];
-      console.log(gameId);
+      return gameId;
     });
 };
 
