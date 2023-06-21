69c56c0a2231f4997b98c16c6bcf24d2dd1c4285
rbd3 <rabedesanaa@gmail.com>
2023-06-21 17:28:01 +0300

    Add refresh and submit action

---
+++ modules/gameId.js
@@ -0,0 +1,22 @@
+const gameId = () => {
+    fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name: "Project Game",
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        const gameId = json.result.match(/Game with ID: (\w+) added/)[1];
+        console.log(gameId);
+      });
+  };
+  
+   gameId();
+  
